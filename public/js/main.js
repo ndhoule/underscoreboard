e3a9a70b208bf7dca1683a1e71109bb3ea075f81
Nathan Houle <nathan@nathanhoule.com>
2013-01-11 17:30:23 -0800

    Basic websocket exchange working

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,35 @@
-// Generate uid
-var uid = Math.random();
-// Instantiate both editor sessions
-var editor1 = ace.edit("editor1");
-editor1.setTheme("ace/theme/monokai");
-editor1.getSession().setMode("ace/mode/javascript");
-var editor2 = ace.edit("editor2");
-editor2.setTheme("ace/theme/monokai");
-editor2.getSession().setMode("ace/mode/javascript");
+/*global "ace":false, "io":false*/
 
-// Lay down dat socket.io connection
-var socket = io.connect();
+(function(){
+  "use strict";
 
-socket.on('error', function (e){
-  console.error('Unable to create Socket.io connection. Error: ', e);
-});
+  // Generate uid
+  var uid = Math.random();
+  // Instantiate both editor sessions
+  var editor1 = ace.edit("editor1");
+  editor1.setTheme("ace/theme/monokai");
+  editor1.getSession().setMode("ace/mode/javascript");
+  var editor2 = ace.edit("editor2");
+  editor2.setTheme("ace/theme/monokai");
+  editor2.getSession().setMode("ace/mode/javascript");
+  editor2.setReadOnly(true);
 
-socket.on('connect', function (){
-  console.info('Socket.io connection established');
-});
+  // Lay down dat socket.io connection
+  var socket = io.connect();
 
-socket.on('replaceEditor', function(message){
-  editor2.setValue(message);
-});
+  socket.on('error', function (e){
+    console.error('Unable to create Socket.io connection. Error: ', e);
+  });
 
-editor1.on("change", function(){
-  socket.emit('editorContents', editor1.getValue());
-});
+  socket.on('connect', function (){
+    console.info('Socket.io connection established');
+  });
 
+  socket.on('replaceEditor', function(message){
+    editor2.setValue(message);
+  });
+
+  editor1.on("change", function(){
+    socket.emit('editorContents', editor1.getValue());
+  });
+})();

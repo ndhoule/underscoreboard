91c9f3096382cda47c19fc06af419633dbdecaf9
Nathan Houle <nathan@nathanhoule.com>
2013-01-12 11:08:29 -0800

    General restructuring, added autorefresh for tests

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,42 @@
-/*global "ace":false, "io":false*/
+/*global 'ace':false, 'io':false, 'setTimeout':false, 'document':false, 'window':false, 'console':false*/
 
 (function(){
   "use strict";
 
-  // Generate uid
-  var uid = Math.random();
-  // Instantiate both editor sessions
-  var editor1 = ace.edit("editor1");
-  editor1.setTheme("ace/theme/monokai");
-  editor1.getSession().setMode("ace/mode/javascript");
-  var editor2 = ace.edit("editor2");
-  editor2.setTheme("ace/theme/monokai");
-  editor2.getSession().setMode("ace/mode/javascript");
-  editor2.setReadOnly(true);
+  var createEditor = function(div, editable){
+    var editor = ace.edit(div);
+    if (editable !== true) {
+      editor.setReadOnly(true);
+    }
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/javascript");
+    editor.getSession().setTabSize(2);
+    return editor;
+  };
+
+  var updateCount = 0;
+
+  var update = function(){
+    if (--updateCount === 0) {
+      document.getElementById('editor1-tests').contentDocument.location.reload(true);
+    }
+  };
+
+
+  /* CONSUMPTION CODE */
+  // Instantiate editor sessions
+  var editor1 = createEditor('editor1', true);
+  var editor2 = createEditor('editor2');
+  window.xeditor1 = editor1;
 
   // Lay down dat socket.io connection
   var socket = io.connect();
 
-  socket.on('error', function (e){
+  socket.on('error', function(e){
     console.error('Unable to create Socket.io connection. Error: ', e);
   });
 
-  socket.on('connect', function (){
+  socket.on('connect', function(){
     console.info('Socket.io connection established');
   });
 
@@ -30,6 +45,9 @@
   });
 
   editor1.on("change", function(){
+    updateCount++;
     socket.emit('editorContents', editor1.getValue());
+    setTimeout(update, 1200);
   });
+
 })();

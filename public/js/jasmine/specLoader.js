23b5a28b3a7d1fae72110e7dbcea2d0487385183
Nathan Houle <nathan@nathanhoule.com>
2013-01-12 16:55:03 -0800

    Separated eval logic from specrunner, general improvements

---
+++ public/js/jasmine/specLoader.js
@@ -0,0 +1,41 @@
+(function() {
+  // Unmap the function we're asking the user to implement
+  _.each = _.forEach = undefined;
+
+  // If the user provides a leading var keyword, we need to strip it before
+  // we eval their code; otherwise, it won't be executed in the correct context
+  _.each = each = eval(parent.xeditor1.getValue().replace(/^var /g, ''));
+
+  if(!_.each){
+    if(!window.each){
+      throw new Error('You must define an each function either in the underscore namespace or in the global scope');
+    }
+    _.each = window.each;
+  }
+
+  // Jasmine boilerplate
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var htmlReporter = new jasmine.HtmlReporter();
+
+  jasmineEnv.addReporter(htmlReporter);
+
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  var currentWindowOnload = window.onload;
+
+  window.onload = function() {
+    if (currentWindowOnload) {
+      currentWindowOnload();
+    }
+    execJasmine();
+  };
+
+  function execJasmine() {
+    jasmineEnv.execute();
+  }
+
+})();

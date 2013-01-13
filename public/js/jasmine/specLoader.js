1162625b6bc74b05f0fc6b6f2f2b999060bac04a
Nathan Houle <nathan@nathanhoule.com>
2013-01-12 18:17:16 -0800

    Refactored editor parsing to be a bit more clear

--- public/js/jasmine/specLoader.js
+++ public/js/jasmine/specLoader.js
@@ -1,10 +1,17 @@
+/*jshint evil:true*/
+
 (function() {
   // Unmap the function we're asking the user to implement
   _.each = _.forEach = undefined;
 
+
   // If the user provides a leading var keyword, we need to strip it before
   // we eval their code; otherwise, it won't be executed in the correct context
-  _.each = each = eval(parent.xeditor1.getValue().replace(/^var /g, ''));
+  var editorContents = parent.xeditor1.getValue();
+  editorContents = editorContents.replace(/^var /g, '');
+
+  // Here be eval dragons
+  _.each = each = eval(editorContents);
 
   if(!_.each){
     if(!window.each){

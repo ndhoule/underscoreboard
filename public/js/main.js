8bf167eaeacbf7a5d95b0bee18015851c8dcd1a6
Nathan Houle <nathan@nathanhoule.com>
2013-01-15 16:26:08 -0800

    Scripts now wait for doc ready before modifying DOM

--- public/js/main.js
+++ public/js/main.js
@@ -2,58 +2,61 @@
 
 require.config({
   paths: {
-    ace: '/js/lib/ace',
-    io : '/socket.io/socket.io.js'
+    ace     : '/js/lib/ace',
+    io      : '/socket.io/socket.io.js',
+    domReady: '/js/lib/domReady'
   }
 });
 
-require(['jquery', 'io', 'ace/ace'], function($, io, ace) {
+require(['domReady', 'jquery', 'io', 'ace/ace'], function(domReady, $, io, ace) {
   "use strict";
+  domReady(function () {
+    var updateCount = 0;
 
-  var createEditor = function(div, editable){
-    var editor = ace.edit(div);
-    if (editable !== true) {
-      editor.setReadOnly(true);
-    }
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
-    editor.getSession().setTabSize(2);
-    return editor;
-  };
+    var update = function(){
+      if (--updateCount === 0) {
+        document.getElementById('editor1-tests').contentDocument.location.reload(true);
+      }
+    };
 
-  var updateCount = 0;
-
-  var update = function(){
-    if (--updateCount === 0) {
-      document.getElementById('editor1-tests').contentDocument.location.reload(true);
-    }
-  };
+    var createEditor = function(div, editable){
+      var editor = ace.edit(div);
+      if (editable !== true) {
+        editor.setReadOnly(true);
+      }
+      editor.setTheme("ace/theme/monokai");
+      editor.getSession().setMode("ace/mode/javascript");
+      editor.getSession().setTabSize(2);
+      return editor;
+    };
 
 
-  /* CONSUMPTION CODE */
-  // Instantiate editor sessions
-  var editor1 = createEditor('editor1', true);
-  var editor2 = createEditor('editor2');
-  window.xeditor1 = editor1;
+    /* CONSUMPTION CODE */
+    // Instantiate editor sessions
+    var editor1 = createEditor('editor1', true);
+    var editor2 = createEditor('editor2');
+    window.xeditor1 = editor1;
 
-  // Lay down dat socket.io connection
-  var socket = io.connect();
+    // Lay down dat socket.io connection
+    var socket = io.connect();
 
-  socket.on('error', function(e){
-    console.error('Unable to create Socket.io connection. Error: ', e);
-  });
+    socket.on('error', function(e){
+      console.error('Unable to create Socket.io connection. Error: ', e);
+    });
 
-  socket.on('connect', function(){
-    console.info('Socket.io connection established');
-  });
+    socket.on('connect', function(){
+      console.info('Socket.io connection established');
+    });
 
-  socket.on('replaceEditor', function(message){
-    editor2.setValue(message);
-  });
+    socket.on('replaceEditor', function(message){
+      editor2.setValue(message);
+    });
 
-  editor1.on("change", function(){
-    updateCount++;
-    socket.emit('editorContents', editor1.getValue());
-    setTimeout(update, 1200);
+    editor1.on("change", function(){
+      updateCount++;
+      socket.emit('editorContents', editor1.getValue());
+      setTimeout(update, 1200);
+    });
+
   });
 });

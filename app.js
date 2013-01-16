27dcd8216760264d088e61e30b463d462f68262b
Nathan Houle <nathan@nathanhoule.com>
2013-01-15 21:57:16 -0800

    Refactored server to use require.js

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 /*jshint node:true, laxcomma:true*/
 "use strict";
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path')
-  , io = require('socket.io');
+var requirejs = require('requirejs');
+requirejs.config({nodeRequire: require});
 
-// Create a server and attach socket.io to it
-var app = express()
-  , server = http.createServer(app)
-  , io = io.listen(server, {origins: '*:*', log: false});
+requirejs(['http', 'path', 'express', './routes', 'socket.io'], function (http, path, express, routes, socketio) {
+  var app = express()
+    , server = http.createServer(app)
+    , io = socketio.listen(server, {origins: '*:*', log: false});
 
-
-app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.get('/', routes.index);
-
-io.configure(function (){
-  io.set('authorization', function (handshakeData, cb) {
-    cb(null, true);
-  });
-});
-
-io.sockets.on('connection', function (client) {
-  client.on('editorContents', function(message) {
-    client.broadcast.emit('replaceEditor', message);
+  app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
   });
 
-  client.on('disconnect',function(){
-    console.log('Server has disconnected');
+  app.get('/', routes.index);
+
+  io.configure(function (){
+    io.set('authorization', function (handshakeData, cb) {
+      cb(null, true);
+    });
   });
-});
 
-server.listen(app.get('port'), function() {
-  console.log("Underscoreboard server listening on port " + app.get('port'));
-});
+  io.sockets.on('connection', function (client) {
+    client.on('editorContents', function(message) {
+      client.broadcast.emit('replaceEditor', message);
+    });
 
+    client.on('disconnect',function(){
+      console.log('Server has disconnected');
+    });
+  });
+
+
+  server.listen(app.get('port'), function() {
+    console.log("Underscoreboard server listening on port " + app.get('port'));
+  });
+
+});

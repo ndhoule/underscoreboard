1bcc154aff879e0f27f09fea29ec8dac42cd60b2
Nathan Houle <nathan@nathanhoule.com>
2013-01-20 10:05:05 -0800

    Improved user input regex and added todo

--- public/js/mocha/spec/underscoreSpecHelper.js
+++ public/js/mocha/spec/underscoreSpecHelper.js
@@ -5,7 +5,12 @@
   // If the user provides a leading var keyword, we need to strip it before
   // we eval their code; otherwise, it won't be executed in the correct context
   var editorContents = parent.xeditor1.getValue();
-  editorContents = editorContents.replace(/^var /g, '');
+  // TODO: This sort of works for the moment: It does lazy matching, so it only
+  // grabs the first var in the file and strips it. Once the server passes in
+  // the name of our function we should concat it in so that a user can declare
+  // a var before their underscore function without breaking the jasmine tests.
+  // Not sure why they'd want to do this, but let's be nice to our users!
+  editorContents = editorContents.replace(/([\s\S])*?^var /m, '');
 
   // Here be eval dragons
   _.each = each = eval(editorContents);

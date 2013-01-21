9ed0952d5a6a7e4f535a5e3a4ba0d2a27e5c9864
Nathan Houle <nathan@nathanhoule.com>
2013-01-21 12:27:30 -0800

    Added basic gruntfile and expanded package.json

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    meta : {
+      pkg    : grunt.file.readJSON('package.json'),
+      banner : '/*! <%= meta.pkg.title || meta.pkg.name %> - v<%= meta.pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= meta.pkg.homepage ? "* " + meta.pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= meta.pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(meta.pkg.licenses, "type").join(", ") %> */\n',
+      src    : ['server/app.js', 'server/routes/**/*', 'client/js/main.js', 'client/js/createEditor.js', 'client/js/mocha/spec/**/*'],
+      vendor : ['lib/underscore/underscore.js', 'lib/jquery/jquery-latest.js', 'src/undermore.js'],
+      specs  : ['specs/**/*Spec.js']
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        '<%= meta.src %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  // Load third-party modules
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Define tasks
+  grunt.registerTask('test', ['jshint']);
+
+  // Define default task
+  grunt.registerTask('default', ['test']);
+};

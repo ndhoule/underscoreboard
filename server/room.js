a1c4c492258a8ed6b376c8ce923a1421030aefd3
Nathan Houle <ndhoule@gmail.com>
2013-01-27 21:45:38 -0800

    Moved Room constructor function into module

---
+++ server/room.js
@@ -0,0 +1,86 @@
+/*jshint node:true, laxcomma:true*/
+/*globals define: true*/
+"use strict";
+
+define(['underscore', './functions.json'], function (_, fns) {
+  // Room constructor function
+  // TODO: Get rid of io arg as dependency
+  return function(io){
+    var roomID = makeID(10);
+    var users = [];
+    var currentFn = null;
+
+    function makeID(len){
+        var CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var id = "";
+        for (var i = 0, idLen = len || 10; i < idLen; i++) {
+          id += CHARSET.charAt(Math.floor(Math.random() * CHARSET.length));
+        }
+        return id;
+    }
+
+    return {
+
+      initGame: function(){
+        console.log('starting game id ' + roomID);
+        currentFn = this.genRandomFn();
+        io.sockets.in(roomID).emit('beginGame', currentFn);
+      },
+
+     genRandomFn: function(){
+        var randProp,
+            randIndex,
+            keys = [];
+
+        _.each(fns, function(val, prop){
+          keys.push(prop);
+        });
+
+        randIndex = Math.floor(Math.random() * _.size(keys));
+        randProp = keys[randIndex];
+
+        return fns[randProp];
+      },
+
+      // Checks if the room is full. Returns true if yes, false if no.
+      isFull: function(){
+        return users.length >= 2;
+      },
+
+      // Adds a user to the room's users array and subscribes them to this
+      // room's broadcasts.
+      addUser: function(user){
+        // TODO: Fix this security hole--isFull could be redefined by user
+        if ( this.isFull() ) { throw new Error("Cannot add users to a full room."); }
+
+        // Subscribe a user to this room's socket broadcasts
+        user.getSocket().join(roomID);
+        return users.push(user);
+      },
+
+      // Remove a user from the current room.
+      removeUser: function(user){
+        //TODO: Implement garbage collection on rooms
+      },
+
+      // Return an array containing all users in the room.
+      getUsers: function(){
+        return users;
+      },
+
+      getID: function(){
+        return roomID;
+      },
+
+      // Broadcast the contents of a user's editor to that user's room.
+      updateEditor: function(data, socket){
+        socket.broadcast.to(roomID).emit('updateEditor', data);
+      },
+
+      sweetVictory: function(data, socket){
+        socket.broadcast.to(roomID).emit('sweetVictory', data);
+      }
+
+    };
+  };
+});
--- server/web.js
+++ server/web.js
@@ -4,94 +4,13 @@
 var requirejs = require('requirejs');
 requirejs.config({nodeRequire: require});
 
-requirejs(['./app', 'http', './routes', 'socket.io', 'underscore', './functions.json'], function (app, http, routes, socketio, _, fns) {
+requirejs(['./app', 'http', './routes', './room', 'socket.io'], function (app, http, routes, Room, socketio) {
   var server = http.createServer(app)
     , io = socketio.listen(server, {origins: '*:*', log: false});
 
+  // Routing table
   app.get('/', routes.index);
 
-
-  // ID generator utility function
-  var makeID = function(len) {
-      var CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      var id = "";
-
-      for (var i = 0, idLen = len || 10; i < idLen; i++) {
-        id += CHARSET.charAt(Math.floor(Math.random() * CHARSET.length));
-      }
-
-      return id;
-  };
-
-  // Room constructor function
-  var Room = function(){
-    var roomID = makeID(10);
-    var users = [];
-    var currentFn = null;
-    return _.extend(Object.create(Room), {
-
-      initGame: function(){
-        console.log('starting game id ' + roomID);
-        currentFn = this.genRandomFn();
-        io.sockets.in(roomID).emit('beginGame', currentFn);
-      },
-
-     genRandomFn: function(){
-        var randProp,
-            randIndex,
-            keys = [];
-
-        _.each(fns, function(val, prop){
-          keys.push(prop);
-        });
-
-        randIndex = Math.floor(Math.random() * _.size(keys));
-        randProp = keys[randIndex];
-
-        return fns[randProp];
-      },
-
-      // Checks if the room is full. Returns true if yes, false if no.
-      isFull: function(){
-        return users.length >= 2;
-      },
-
-      // Adds a user to the room's users array and subscribes them to this
-      // room's broadcasts.
-      addUser: function(user){
-        // TODO: Fix this security hole--isFull could be redefined by user
-        if ( this.isFull() ) { throw new Error("Cannot add users to a full room."); }
-
-        // Subscribe a user to this room's socket broadcasts
-        user.getSocket().join(roomID);
-        return users.push(user);
-      },
-
-      // Remove a user from the current room.
-      removeUser: function(user){
-        //TODO: Implement garbage collection on rooms
-      },
-
-      // Return an array containing all users in the room.
-      getUsers: function(){
-        return users;
-      },
-
-      getID: function(){
-        return roomID;
-      },
-
-      // Broadcast the contents of a user's editor to that user's room.
-      updateEditor: function(data, socket){
-        socket.broadcast.to(roomID).emit('updateEditor', data);
-      },
-
-      sweetVictory: function(data, socket){
-        socket.broadcast.to(roomID).emit('sweetVictory', data);
-      }
-
-    });
-  };
 
   var User = function(socket){
     // TODO: More robust checking here
@@ -127,12 +46,12 @@ requirejs(['./app', 'http', './routes', 'socket.io', 'underscore', './functions.
   var Users = {};
 
   // Create a room when we initialize the server
-  var currentRoom = Room();
+  var currentRoom = Room(io);
 
   io.sockets.on('connection', function (socket) {
     // Check to see if the current room is full and create a new one if it is
     if( currentRoom.isFull() ){
-      currentRoom = Room();
+      currentRoom = Room(io);
     }
 
     // Create a user and link it to its corresponding socket connection

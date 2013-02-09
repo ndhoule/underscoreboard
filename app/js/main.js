1b79f7ab712477899b51dc5f5ce47620d59daafa
Nathan Houle <ndhoule@gmail.com>
2013-02-08 21:38:01 -0800

    More variable renaming for consistency, clarity

--- app/js/main.js
+++ app/js/main.js
@@ -20,19 +20,24 @@ require.config({
 require(['domReady', 'jquery', 'io', 'createEditor', 'bootstrap'], function(domReady, $, io, createEditor) {
   "use strict";
 
-  // Initialize a few necessary globals and put them in the underscoreboardGlobals
-  // namespace
-  window.underscoreboardGlobals = {
-    specFailures: null,
-    playerEditor: null,
-    currentFunction: null
-  };
-
   // The contents of this file should only load once the DOM is ready, so wrap
   // them in require.js's equivalent of $(document).ready
   domReady(function() {
-    var currentFn,
+    var currentFunction,
         updateCount = 0;
+
+    var editors = {
+      player: createEditor('editor-player'),
+      opponent: createEditor('editor-opponent', true)
+    };
+
+    // Create a few globals so we can share values with the testing iframe.
+    // Namespace them in the underscoreboardGlobals object to prevent collisions
+    window.underscoreboardGlobals = {
+      specFailures: null,
+      playerEditor: editors.player,
+      currentFunction: null
+    };
 
     var updateTests = function() {
       if (--updateCount === 0) {
@@ -47,13 +52,6 @@ require(['domReady', 'jquery', 'io', 'createEditor', 'bootstrap'], function(domR
       }
     };
 
-    var editors = {};
-    editors.p = createEditor('editor-p');
-    editors.o = createEditor('editor-o', true);
-
-    // Declare a global to make the player's editor available to the testing iframe.
-    window.underscoreboardGlobals.playerEditor = editors.p;
-
     // Show loading menu on pageload
     setTimeout(function() {
       $('#pairing-modal').modal('show');
@@ -63,7 +61,7 @@ require(['domReady', 'jquery', 'io', 'createEditor', 'bootstrap'], function(domR
       e.preventDefault();
       if (window.confirm("Are you sure you want to reset your code to the start point?")) {
         //TODO: dry
-        editors.p.setValue(currentFn.desc.join('\n') + '\n' + currentFn.boiler.join('\n'));
+        editors.player.setValue(currentFunction.desc.join('\n') + '\n' + currentFunction.boiler.join('\n'));
       }
     });
 
@@ -81,39 +79,39 @@ require(['domReady', 'jquery', 'io', 'createEditor', 'bootstrap'], function(domR
     socket.on('beginGame', function(message) {
       setTimeout(function() {
         // Make both a local and global reference to the current function
-        window.underscoreboardGlobals.currentFunction = currentFn = message;
+        window.underscoreboardGlobals.currentFunction = currentFunction = message;
 
         // Insert the placeholder text into editor and move cursor to the start point
-        editors.p.setValue(currentFn.desc.join('\n') + '\n' + currentFn.boiler.join('\n'));
-        editors.p.selection.moveCursorBy(-1, 0);
-        editors.p.selection.clearSelection();
+        editors.player.setValue(currentFunction.desc.join('\n') + '\n' + currentFunction.boiler.join('\n'));
+        editors.player.selection.moveCursorBy(-1, 0);
+        editors.player.selection.clearSelection();
 
         // Hide the victory/loss modals if they are displayed
         $('#victory-modal').modal('hide');
         $('#loss-modal').modal('hide');
 
         // Display the current function to the user and load the test URL
-        $('#current-function-name').html('<small>' + currentFn.name + '</small>');
+        $('#current-function-name').html('<small>' + currentFunction.name + '</small>');
         $('#current-function-label').show();
         $('#current-function-name').show();
         $('#pairing-modal').modal('hide');
-        $('#tests').attr({'src': '/mocha/SpecRunner.html?grep=_.' + currentFn.name});
+        $('#tests').attr({'src': '/mocha/SpecRunner.html?grep=_.' + currentFunction.name});
       }, 3000);
     });
 
     socket.on('updateEditor', function(message) {
-      editors.o.setValue(message);
+      editors.opponent.setValue(message);
       // Fixes annoying highlighting of opponent's editor when its contents changes
-      editors.o.selection.clearSelection();
+      editors.opponent.selection.clearSelection();
     });
 
     socket.on('sweetVictory', function(message) {
       $('#loss-modal').modal('show');
     });
 
-    editors.p.on('change', function() {
+    editors.player.on('change', function() {
       updateCount++;
-      socket.emit('editorChange', editors.p.getValue());
+      socket.emit('editorChange', editors.player.getValue());
       setTimeout(updateTests, 1200);
       setTimeout(verifyTests, 2500);
     });

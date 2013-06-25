4c5679e121aa1a5c32fd18c40619df0c01c93ff5
Nathan Houle <nathan@nathanhoule.com>
2013-06-25 01:25:08 -0700

    Remove unused SockJS server-side config options

--- app/config/sockjs_server.js
+++ app/config/sockjs_server.js
@@ -2,10 +2,6 @@
   'use strict';
 
   define(['sockjs'], function (sockjs) {
-    var sockjs_opts = {
-      sockjs_url: 'http://cdn.sockjs.org/sockjs-0.3.min.js'
-    };
-
-    return sockjs.createServer(sockjs_opts);
+    return sockjs.createServer();
   });
 }());
